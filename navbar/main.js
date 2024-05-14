7d534f4c64980af77bcd31db55f9aa4af5361e81
osh0612 <105645937+osh0612@users.noreply.github.com>
2024-05-14 07:49:18 -0700

    Create main.js

---
+++ navbar/main.js
@@ -0,0 +1,17 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
